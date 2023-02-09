efad68c2273b7857449c41c0e1501acdc1d59636
hanichris <hanichris71@gmail.com>
2023-02-10 02:57:04 +0300

    compute average score.

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+-- Compute students average score
+DELIMITER $$
+CREATE
+PROECDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
+BEGIN
+	 SET @avg_score = SELECT AVG(score) FROM corrections WHERE user_id=user_id;
+	 UPDATE users
+	 SET average_score = @avg_score WHERE id = user_id;
+END$$
+DELIMITER ;
