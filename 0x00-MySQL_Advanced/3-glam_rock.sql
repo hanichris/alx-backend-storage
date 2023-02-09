ac2a2c04b9603a195583b1ead14b5f15734f8621
hanichris <hanichris71@gmail.com>
2023-02-10 00:27:19 +0300

    set default date to 2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Show bands with a particular style and their lifespan
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
