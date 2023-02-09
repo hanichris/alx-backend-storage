3ce7cae9ee8efa93f49632b696f3d6cbace00bc4
hanichris <hanichris71@gmail.com>
2023-02-09 22:23:56 +0300

    remove misplaced comma

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- id, email and name.
 CREATE TABLE IF NOT EXISTS user (
     id int NOT NULL AUTO_INCREMENT,
-    email varchar(255), NOT NULL UNIQUE,
+    email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
     PRIMARY KEY (id)
 );
