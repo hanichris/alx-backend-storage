31b8bdc7bdfe158d7b55d4cfdb57d06018e9a8f8
hanichris <hanichris71@gmail.com>
2023-02-09 22:19:56 +0300

    sql script to create a user table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Create a user table with the following fields:
+-- id, email and name.
+CREATE TABLE IF NOT EXISTS user (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255), NOT NULL UNIQUE,
+    name varchar(255),
+    PRIMARY KEY (id)
+);
