79b772cc257c17b7e8b1b5ebeb775144ca9bcce6
hanichris <hanichris71@gmail.com>
2023-02-09 23:46:16 +0300

    sum to compute the total.

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Rank country of orgin of bands ordered by number of non-unique fans.
-SELECT origin, COUNT(fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
