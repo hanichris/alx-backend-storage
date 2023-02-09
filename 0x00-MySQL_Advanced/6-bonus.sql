720ddd35580cbebb15b6eaff041504f8859351f5
hanichris <hanichris71@gmail.com>
2023-02-10 02:17:37 +0300

    using a proecdure

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+-- Create a procedure `AddBonus that adds a new correction.
+DELIMITER $$
+CREATE
+PROCEDURE AddBonus (
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT)
+BEGIN
+  IF NOT EXISTS (SELECT name FROM projects WHERE name = project_name) THEN
+      INSERT INTO projects(name) VALUES (project_name);
+  ENDIF;
+
+  INSERT INTO corrections (user_id, project_id, score)
+  VALUES (user_id, (SELECT project_id FROM projects WHERE name = project_name), score);
+END$$
