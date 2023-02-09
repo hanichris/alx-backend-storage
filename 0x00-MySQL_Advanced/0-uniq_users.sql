6455cd9e68f827b8b8914466a20e42d9cb6f0cb9
hanichris <hanichris71@gmail.com>
2023-02-09 22:26:35 +0300

    `users` not `user`

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create a user table with the following fields:
 -- id, email and name.
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
