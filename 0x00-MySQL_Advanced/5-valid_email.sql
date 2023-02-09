84bbf81c80ad1ac9d5d204c50d939e815cd50a5a
hanichris <hanichris71@gmail.com>
2023-02-10 01:26:07 +0300

    validate an email update.

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Reset attribute `valid_email` only when `email` has been changed.
+DELIMITER $$
+CREATE
+TRIGGER update_email
+BEFORE UPDATE
+ON users FOR EACH ROW
+BEGIN
+IF OLD.email <> NEW.email
+THEN
+SET NEW.valid_email = 0;
+ENDIF;
+END
