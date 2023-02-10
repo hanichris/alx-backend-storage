504b4ce9aa7a6ae2df4542221bcbe3f028af3ab5
hanichris <hanichris71@gmail.com>
2023-02-10 03:09:55 +0300

    no use of local variables

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,8 +6,8 @@ PROCEDURE ComputeAverageScoreForUser (
     IN user_id INT
 )
 BEGIN
-SET @avg_score = (SELECT AVG(score) FROM corrections WHERE user_id=user_id);
 UPDATE users
-SET average_score = @avg_score WHERE id = user_id;
+SET average_score = (SELECT AVG(score) FROM collections WHERE user_id=user_id)
+WHERE id = user_id;
 END$$
 DELIMITER ;
