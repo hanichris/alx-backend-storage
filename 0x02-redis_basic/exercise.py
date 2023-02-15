640e195ad1c77f4c708ab3c13aa20348f2eafd65
hanichris <hanichris71@gmail.com>
2023-02-16 01:17:59 +0300

    Added 3 methods: get, get_str and get_int.
    get retrieves the value of the passed key and converts it to the desired format.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module that explores working with the Redis data structure store."""
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -24,3 +24,30 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Retrieve an element from the Redis data structure store.
+
+        Args:
+            key (str): id for the value in the Redis store.
+            fn (Callable): function callable to convert the return
+                type of the get method of redis.Redis() from bytes.
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Parametrize Cache.get to string."""
+        return self.get(key).decode("utf-8")
+
+    def get_int(self, key:str) -> int:
+        """Parametrize Cache.get to int."""
+        value = self.get_str(key)
+        try:
+            value = int(value)
+        except Exception:
+            value = 0
+        return value
